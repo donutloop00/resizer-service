2e2c07f3dc407ec96d3db25cebc1fa36ebe59979
Patrick Namasondhi <pnamasondhi@gmail.com>
2018-10-11 15:43:38 -0700

    added main server file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	//"github.com/labstack/echo"
+	//	"github.com/h2non/imaginary"
+)
+
+func main() {
+	//	res, err := http.Post()
+	resp, err := http.Get("imaginary:9000/form")
+	if err != nil {
+		//replace with a legit error later
+		fmt.Println("trouble reaching imaginary server")
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	//	http.ListenAndServe(":8080")
+}
