da5ba618bf7c4ef9fe084061abae325ad5d2dc19
Patrick Namasondhi <pnamasondhi@gmail.com>
2018-10-15 22:35:19 -0700

    updated imgDefaultHandler function signature and route paths

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/labstack/echo"
 	"io/ioutil"
 	"net/http"
-	//"github.com/labstack/echo"
 	//	"github.com/h2non/imaginary"
 )
 
@@ -21,21 +22,28 @@ func sendRequestToImaginary() {
 
 }
 
-func imgDefaultHandler(c echo.Context) {
+func imgDefaultHandler(c echo.Context) error {
+	//e.GET("/:rawImagePath/:resizedImageName", imgDefaultHandler)
+
+	//multiple images that have the same filename
+	//to deal with multiple images that might have the same name, I tihnk it'd be worthwhile to hash the function. I know I'd have to worry about namespace collisions in a real-world application but
+
+	//images of different aspect ratios and original sizes
+
 	//run req validator function here and respond with error early if needed
-
-}
-
-func imgResizeHandler(c echo.Context) {
-	//run req validator function here and respond with error early if needed
-
+	err := validateRequestContext(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
+	//	c.JSON(http.StatusBadRequest, jsonErrorResPayload)
 }
 
 func main() {
 	e := echo.New()
 
-	e.GET("/path1", imgDefaultHandler)
-	e.GET("/path2", imgResizeHandler)
+	e.GET("/:rawImagePath/:resizedImageName", imgDefaultHandler)
+	e.GET("/:resizedImagePath/:resizedImageName", imgResizeHandler)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
