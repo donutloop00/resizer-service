86ac1524c184c8b9ae361bbe0303d116535c6902
Patrick Namasondhi <pnamasondhi@gmail.com>
2018-10-16 15:35:07 -0700

    removed route handler and replaced with static file middleware

--- main.go
+++ main.go
@@ -30,28 +30,10 @@ func checkValidDimensions(x, y int) bool {
 	}
 }
 
-func imgDefaultHandler(c echo.Context) error {
-// 	//e.GET("/:rawImagePath/:resizedImageName", imgDefaultHandler)
-
-// 	//multiple images that have the same filename
-	//to deal with multiple images that might have the same name, I tihnk it'd be worthwhile to hash the function. I know I'd have to worry about namespace collisions in a real-world application but
-
-	//images of different aspect ratios and original sizes
-
-	//run req validator function here and respond with error early if needed
-	// err := validateRequestContext(c)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	return nil
-	//	c.JSON(http.StatusBadRequest, jsonErrorResPayload)
-}
-
 func main() {
 	e := echo.New()
 
-	e.GET("/:rawImagePath/:resizedImageName", imgDefaultHandler)
-	// e.GET("/:resizedImagePath/:resizedImageName", imgResizeHandler)
+	e.Static("/static", "./static")
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
