8af7a427185d0c3c062ea4c7762380116b36b662
Patrick Namasondhi <pnamasondhi@gmail.com>
2018-10-18 09:52:58 -0700

    changed filename delimiter to - and updated resizer server address to match docker-compose local DNS

--- main.go
+++ main.go
@@ -7,26 +7,24 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"io/ioutil"
 	"net/http"
-	//	"github.com/h2non/imaginary"
 )
 
 func sendRequestToResizer(fileHandle *os.File, x int, y int) (string, error) {
 	splitFileName := strings.Split(fileHandle.Name(), ".")
 	tokenizedPathNoExt := strings.Split(splitFileName[0], "/")
-	baseFileName := tokenizedPathNoExt[len(tokenizedPathNoExt) - 1]
+	baseFileName := tokenizedPathNoExt[len(tokenizedPathNoExt)-1]
 	imageType := splitFileName[len(splitFileName) - 1]
 	contentType := "image/" + imageType
 
 	widthAsStr := strconv.Itoa(x)
 	heightAsStr := strconv.Itoa(y)
 
-	//width is the only required dimension for a resize
-	resizeQueryStr := "width=" + widthAsStr
+	resizeQueryStr := "width=" + widthAsStr + "&height=" + heightAsStr
 
-	resp, err := http.Post("http://localhost:9000/resize?" + resizeQueryStr, contentType, fileHandle)
-	// resp, err := http.Post("imaginary/resize?" + resizeQueryStr, contentType, fileHandle)
+	resp, err := http.Post("http://imaginary:9000/resize?"+resizeQueryStr, contentType, fileHandle)
 	if err != nil {
 		errorMsg := "Error communicating with image resize server."
 		return "", echo.NewHTTPError(http.StatusInternalServerError, errorMsg)
@@ -42,11 +40,11 @@ func sendRequestToResizer(fileHandle *os.File, x int, y int) (string, error) {
 	fmt.Println(fileHandle.Name())
 
 	pathOfResizedImage := "static/resized/" + baseFileName + "_" + widthAsStr + "_" + heightAsStr + "." + imageType
-    err = ioutil.WriteFile(pathOfResizedImage, body, 0666)
-    if err != nil {
+	err = ioutil.WriteFile(pathOfResizedImage, body, 0666)
+	if err != nil {
 		errorMsg := "Error writing resized image to server disk."
 		return "", echo.NewHTTPError(http.StatusInternalServerError, errorMsg)
-    }
+	}
 
 	return pathOfResizedImage, nil
 }
@@ -54,28 +52,28 @@ func sendRequestToResizer(fileHandle *os.File, x int, y int) (string, error) {
 func checkValidDimensions(x, y int) bool {
 	if x > 0 && y > 0 {
 		return true
-	}	else {
+	} else {
 		return false
 	}
 }
 
 func imgResizeHandler(c echo.Context) error {
 	//check if the image has already been resized before
-	resizedFilename := c.Param("resizedImageName")	
+	resizedFilename := c.Param("resizedImageName")
 
-	//filename format: uniquefilename_x_y.jpg
-	resizedFilenameAndExt := strings.Split(resizedFilename,".")
-	fileExt := resizedFilenameAndExt[len(resizedFilenameAndExt) - 1]
-	tokenizedName := strings.Split(resizedFilenameAndExt[0], "_")
+	//filename format: uniquefilename-x-y.jpg
+	resizedFilenameAndExt := strings.Split(resizedFilename, ".")
+	fileExt := resizedFilenameAndExt[len(resizedFilenameAndExt)-1]
+	tokenizedName := strings.Split(resizedFilenameAndExt[0], "-")
 
 	reqWidth, err := strconv.Atoi(tokenizedName[1])
-	if err != nil {	
+	if err != nil {
 		errorMsg := "Error converting width to an int"
 		return echo.NewHTTPError(http.StatusBadRequest, errorMsg)
 	}
 
 	reqHeight, err := strconv.Atoi(tokenizedName[2])
-	if err != nil {	
+	if err != nil {
 		errorMsg := "Error converting height to an int"
 		return echo.NewHTTPError(http.StatusBadRequest, errorMsg)
 	}
@@ -90,15 +88,15 @@ func imgResizeHandler(c echo.Context) error {
 	// see if the image has been resized before
 	if fileInfo, err := os.Stat("static/resized/" + resizedFilename); !os.IsNotExist(err) {
 		//trusting the server to get the file extension right instead of the user's requested filename
-		fileExt := fileInfo.Name();
-		c.Response().Header().Set(echo.HeaderContentType, resizedFilename + "." + fileExt)
-	  	return c.File("static/resized/" + resizedFilename)
+		fileExt := fileInfo.Name()
+		c.Response().Header().Set(echo.HeaderContentType, resizedFilename+"."+fileExt)
+		return c.File("static/resized/" + resizedFilename)
 	}
 
 	// check if the source image exists. if not, send a 404 error
 	pathOfSourceImage := "static/source/" + tokenizedName[0] + "." + fileExt
 	if _, err := os.Stat(pathOfSourceImage); err != nil {
-		if os.IsNotExist(err){
+		if os.IsNotExist(err) {
 			errorMsg := "Source image does not exist for resizing."
 			return echo.NewHTTPError(http.StatusNotFound, errorMsg)
 		}
@@ -123,6 +121,8 @@ func imgResizeHandler(c echo.Context) error {
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
 
 	e.GET("/static/resized/:resizedImageName", imgResizeHandler)
 	e.Static("/static", "./static")
